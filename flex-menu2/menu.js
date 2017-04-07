e5702f53495c47a406f11900b44dff2823488e1e
simplu27 <simplu27@yahoo.com>
2017-04-07 15:20:59 +0300

    ..

--- flex-menu2/menu.js
+++ flex-menu2/menu.js
@@ -9,13 +9,9 @@ $(document).ready(function(){
 		$('#ham i').toggleClass('fa-bars fa-times');
 		if (nav.hasClass('open')) {
 			nav.removeClass('open');
-			// $(this).html('&#9776;');
 		} else {
 			nav.addClass('open');
-			// $(this).html('&#215;');
 		}
-
-		// $('nav').toggleClass('open');
 	});
 
 });  //end document ready
